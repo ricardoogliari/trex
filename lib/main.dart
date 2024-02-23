2c0f39818e84c3bfe4d02fe83fb11cacd66feade
Ricardo da Silva Ogliari <rogliariping@gmail.com>
2024-02-23 19:21:13 -0300

    first step

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:custom_trex/trex_game.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(
+    GameWidget(game: TRexGame()),
+  );
+}
---
+++ lib/trex_game.dart
@@ -0,0 +1,56 @@
+import 'dart:ui';
+
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+import 'package:flame/text.dart';
+import 'package:flutter/material.dart' hide Image;
+import 'package:flutter/services.dart';
+
+enum GameState { playing, intro, gameOver }
+
+class TRexGame extends FlameGame with KeyboardEvents, TapCallbacks, HasCollisionDetection {
+
+  late final Image spriteImage;
+
+  late final TextComponent scoreText;
+
+  int _score = 0;
+  int _highScore = 0;
+  String scoreString(int score) => score.toString().padLeft(5, '0');
+
+  set score(int newScore) {
+    _score = newScore;
+    scoreText.text = '${scoreString(_score)}  HI ${scoreString(_highScore)}';
+  }
+
+  @override
+  Future<void> onLoad() async {
+    spriteImage = await Flame.images.load('trex.png');
+
+    const chars = '0123456789HI ';
+    final renderer = SpriteFontRenderer.fromFont(
+      SpriteFont(
+        source: spriteImage,
+        size: 23,
+        ascent: 23,
+        glyphs: [
+          for (var i = 0; i < chars.length; i++)
+            Glyph(chars[i], left: 954.0 + 20 * i, top: 0, width: 20),
+        ],
+      ),
+      letterSpacing: 2,
+    );
+    add(
+      scoreText = TextComponent(
+        position: Vector2(20, 20),
+        textRenderer: renderer,
+      ),
+    );
+
+    score = 0;
+  }
+
+}
